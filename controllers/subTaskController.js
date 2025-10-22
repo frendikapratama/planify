72642ae3c7cc196b226cb5c771368cb9a5e50fcb
frendikapratama <frendikapratama28@gmail.com>
2025-10-22 10:07:09 +0700

    final crud subtask

--- controllers/subTaskController.js
+++ controllers/subTaskController.js
@@ -36,9 +36,57 @@ export async function createSubTask(req, res) {
   }
 }
 
-// export async function updateSubTask(req, res) {
-//   try {
-//   } catch (error) {
-//     console.error("Update subtask Error:", error);
-//   }
-// }
+export async function updateSubTask(req, res) {
+  try {
+    const { subTaskId } = req.params;
+    const { taskId, ...updateData } = req.body;
+    const oldSubTask = await Subtask.findById(subTaskId);
+    if (!oldSubTask) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Subtask not found" });
+    }
+    if (taskId && taskId !== String(oldSubTask.task)) {
+      await Task.findByIdAndUpdate(oldSubTask.task, {
+        $pull: { subtask: oldSubTask._id },
+      });
+      await Task.findByIdAndUpdate(taskId, {
+        $push: { subtask: oldSubTask._id },
+      });
+    }
+    const updatedSubTask = await Subtask.findByIdAndUpdate(
+      subTaskId,
+      updateData,
+      { new: true }
+    );
+    res.status(200).json({
+      success: true,
+      message: "subtask updated successfully",
+      data: updatedSubTask,
+    });
+  } catch (error) {
+    console.error("Update subtask Error:", error);
+  }
+}
+
+export async function deleteSubTask(req, res) {
+  try {
+    const { subTaskId } = req.params;
+    const subtask = await Subtask.findByIdAndDelete(subTaskId);
+    if (!subtask) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Subtask not found" });
+    }
+    await Task.findByIdAndUpdate(subtask.task, {
+      $pull: { subtask: subtask._id },
+    });
+    res.status(200).json({
+      success: true,
+      message: "subtask deleted successfully",
+      data: subtask,
+    });
+  } catch (error) {
+    console.error("Delete subtask Error:", error);
+  }
+}
--- routes/subTaskRoutes.js
+++ routes/subTaskRoutes.js
@@ -1,8 +1,15 @@
 import express from "express";
-import { getSubTask, createSubTask } from "../controllers/subTaskController.js";
+import {
+  getSubTask,
+  createSubTask,
+  updateSubTask,
+  deleteSubTask,
+} from "../controllers/subTaskController.js";
 const router = express.Router();
 
 router.get("/", getSubTask);
 router.post("/:taskId", createSubTask);
+router.put("/:subTaskId", updateSubTask);
+router.delete("/:subTaskId", deleteSubTask);
 
 export default router;
