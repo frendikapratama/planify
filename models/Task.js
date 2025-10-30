e6c3128f1496f4353e75d1d6275b991ea5727064
frendikapratama <frendikapratama28@gmail.com>
2025-10-30 11:01:53 +0700

    task version 2

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,10 @@ const taskSchema = new mongoose.Schema(
   {
     nama: { type: String },
     description: String,
-    status: { type: String },
+    status: { type: String, default: "Not Started" },
+    start_date: { type: Date },
+    priority: { type: String },
+    position: { type: Number, default: 0 },
     groups: [{ type: mongoose.Schema.Types.ObjectId, ref: "Group" }],
     subtask: [{ type: mongoose.Schema.Types.ObjectId, ref: "Subtask" }],
   },
